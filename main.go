4f6ea7334985107e7a710a3c9720332e0e831c1d
Keolo Keagy <keolo@kea.gy>
2019-02-02 09:21:21 -0800

    Draft create

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example. V1"
+	title := "Jenkins X golang http example. V5"
 
 	from := ""
 	if r.URL != nil {
